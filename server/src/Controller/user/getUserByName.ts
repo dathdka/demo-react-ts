40802b241a28581945f784eff01007d3d77c7383
dathdka <dathdka@gmail.com>
2023-05-02 12:45:12 +0700

    filter result on server

--- server/src/Controller/user/getUserByName.ts
+++ server/src/Controller/user/getUserByName.ts
@@ -9,13 +9,29 @@ export const getUser: Handler = async (
   try {
     const username = req.query.username;
     const currentPage = req.query.currentPage as string;
+    const isAdmin = req.query.isAdmin as string;
+    const addressKey = req.query.addressKey;
+    let adminCheck =
+      isAdmin === "true" ? true : isAdmin === "false" ? false : null;
+
     const usersHaveName = await user
       .query()
       .where(
         user.raw("LOWER(name)"),
         "like",
-        `%${username?.toString().toLowerCase()}%`)
-        // .andWhere()
+        `%${username?.toString().toLowerCase()}%`
+      )
+      //filter by role
+      .andWhere((builder) => {
+        if (adminCheck === true || adminCheck === false)
+          builder.andWhere("admin", adminCheck);
+      })
+      //filter by address
+      .andWhere(
+        user.raw("LOWER(address)"),
+        "like",
+        `%${addressKey?.toString().toLowerCase()}%`
+      )
       .orderBy("name")
       .page(parseInt(currentPage), 15);
     return res.status(200).json(usersHaveName);
